ea50d887a269f2afa0026ed04acd6f8a1318c013
Morgan Sleeper <zipmon@gmail.com>
2020-12-19 16:24:48 -0600

    Create headershrink.js

---
+++ scripts/headershrink.js
@@ -0,0 +1,12 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+// from https://www.w3schools.com/howto/howto_js_shrink_header_scroll.asp
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("shrinkHeader").style.fontSize = "30px";
+  } else {
+    document.getElementById("shrinkHeader").style.fontSize = "90px";
+  }
+} 
