71313beff4f1c95c218d89ad0ae468b8d8378247
Morgan Sleeper <zipmon@gmail.com>
2020-12-19 18:15:16 -0600

    Update soundonclick.js

--- scripts/soundonclick.js
+++ scripts/soundonclick.js
@@ -36,4 +36,23 @@ function createsoundbite(sound){
 
 //Initialize sound clips:
 
+var aratana=createsoundbite("audio/aratana.mp3")
+var astrob=createsoundbite("audio/astrob.mp3")
+var bellb=createsoundbite("audio/bellb.mp3")
+var gotadelvient=createsoundbite("audio/gotadelvient.mp3")
+var hananohorokobi=createsoundbite("audio/hananohorokobi.mp3")
+var harajukua=createsoundbite("audio/harajukua.mp3")
+var haru=createsoundbite("audio/haru.mp3")
+var melody=createsoundbite("audio/melody.mp3")
+var ogawav1=createsoundbite("audio/ogawav1.mp3")
+var sakurab=createsoundbite("audio/sakurab.mp3")
+var seseragi=createsoundbite("audio/seseragi.mp3")
+var sf1=createsoundbite("audio/sf1.mp3")
+var sh21=createsoundbite("audio/sh21.mp3")
 var sh3=createsoundbite("audio/sh3.mp3")
+var sweetcall=createsoundbite("audio/sweetcall.mp3")
+var thirdman=createsoundbite("audio/thirdman.mp3")
+var uminoeki=createsoundbite("audio/uminoeki.mp3")
+var watercrown=createsoundbite("audio/watercrown/haru.mp3")
+
+
