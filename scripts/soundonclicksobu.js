b75a83d0b5a15c56cfbf04a56cb5b4ac23500573
Morgan Sleeper <zipmon@gmail.com>
2021-02-02 01:06:27 -0600

    One at a time
    Pause any other melodies playing before playing the current one

--- scripts/soundonclicksobu.js
+++ scripts/soundonclicksobu.js
@@ -34,6 +34,13 @@ function createsoundbite(sound){
 	}
 }
 
+function pauseall(){
+	fightingspirita.pause()
+	harunew.pause()
+	sf3.pause()
+	sh6.pause()
+}
+
 //Initialize sound clips:
 
 var fightingspirita=createsoundbite("../audio/fightingspirita.mp3")
