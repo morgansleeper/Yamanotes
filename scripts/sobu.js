2491c930de5bf538a471ecb42eedc9849660e10c
pablopen <penpenotaku@gmail.com>
2021-02-06 18:17:13 +0100

    stop song on second tap and reorganize code

---
+++ scripts/sobu.js
@@ -0,0 +1,13 @@
+function pauseall(){
+	fightingspirita.pause()
+	harunew.pause()
+	sf3.pause()
+	sh6.pause()
+}
+
+//Initialize sound clips:
+
+var fightingspirita=createsoundbite("../audio/fightingspirita.mp3")
+var harunew=createsoundbite("../audio/harunew.mp3")
+var sf3=createsoundbite("../audio/sf3.mp3")
+var sh6=createsoundbite("../audio/sh6.mp3")
---
+++ scripts/uenotokyo.js
@@ -0,0 +1,13 @@
+function pauseall(){
+	bella.pause()
+	beyondtheline.pause()
+	railroadsongb.pause()
+	sunlight.pause()
+}
+
+//Initialize sound clips:
+
+var bella=createsoundbite("../audio/bella.mp3")
+var beyondtheline=createsoundbite("../audio/beyondtheline.mp3")
+var railroadsongb=createsoundbite("../audio/railroadsongb.mp3")
+var sunlight=createsoundbite("../audio/sunlight.mp3")
